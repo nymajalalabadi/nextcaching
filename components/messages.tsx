cbe1401bf91dc4f5b7f9ce487dda48e24ebb5114
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-13 19:39:46 -0600

    start

---
+++ components/messages.tsx
@@ -0,0 +1,10 @@
+export default function Messages({ messages }) {
+    return (
+      <ul className="messages">
+        {messages.map((message) => (
+          <li key={message.id}>{message.text}</li>
+        ))}
+      </ul>
+    );
+  }
+  
